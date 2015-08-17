e3cc547c5fead5eed4601ed31c91b9e113f124a1
Oles Onyshchak <oles1719@gmail.com>
2015-08-17 20:43:06 +0300

    add welcome page

--- web/src/main/java/com/softserve/edu/controller/ParkController.java
+++ web/src/main/java/com/softserve/edu/controller/ParkController.java
@@ -1,16 +1,16 @@
 package com.softserve.edu.controller;
 
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/")
-public class HelloWorldController {
+public class ParkController {
 
-        @RequestMapping(method = RequestMethod.GET)
-        public String viewApplication() {
-            return "index";
-        }
+    @RequestMapping(value = {"/", "/home"})
+    public String showWelcomePage() {
+        return "app/index.html";
     }
+}
