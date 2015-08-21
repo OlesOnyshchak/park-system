7b92f5cf820d1c2f72ae4a7c093301add92f8dce
Oles Onyshchak <oles1719@gmail.com>
2015-08-21 17:54:52 +0300

    add welcome-service

--- web/target/web-1.0-SNAPSHOT/app/js/controllers/welcome-controller.js
+++ web/target/web-1.0-SNAPSHOT/app/js/controllers/welcome-controller.js
@@ -1,11 +1,9 @@
 angular.module('ParkSystem')
-    .controller('WelcomeController', function($scope, $http) {
+    .controller('WelcomeController', ['$scope','WelcomeService', function($scope, WelcomeService) {
         $scope.registrationInfo = {};
         $scope.saveRegistrationInfo = function(reg) {
             console.log(reg);
-            $http.post('registration',reg).success(function(data){
-                console.log("ok");
-            });
+            WelcomeService.create(reg);
         };
-    });
+    }]);
 
